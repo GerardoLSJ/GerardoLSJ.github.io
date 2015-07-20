a70dce32fc72400dfc586a54b47ff996f4bb66e7
GerardoLSJ <LSJGerardo@gmail.com>
2015-07-20 03:28:34 -0500

    wow style

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	 /* $( ".cover" ).on( "mouseover", function() {
+  	$( this ).css( "visibility", "visible" );
+	});
+	$( ".cover" ).on( "mouseleave", function() {
+  	$( this ).css( "visibility", "hidden" );
+	}); */
+
+	$( "#feat" ).click(function() {
+ 	$( "#home" ).removeClass( "wow fadeInDown" );
+	});
+
+
+
+
+
+
+});
