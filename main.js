dc9f1e0df45f945995ef3d3855c775d7361497eb
GerardoLSJ <LSJGerardo@gmail.com>
2015-07-21 16:40:46 -0500

    bouncein

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 $(document).ready(function(){
 
-	 /* $( ".cover" ).on( "mouseover", function() {
-  	$( this ).css( "visibility", "visible" );
-	});
+/*
+	  $( ".cover" ).on( "mouseover", function() {
+  	$( this ).css( "color", "red" );
+	}); 
+
+
 	$( ".cover" ).on( "mouseleave", function() {
-  	$( this ).css( "visibility", "hidden" );
-	}); */
+  	$( this ).css( "color", "green" );
+	}); 
 
 	$( "#feat" ).click(function() {
  	$( "#home" ).removeClass( "wow fadeInDown" );
 	});
 
+*/ var animateEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+	$("#feat").click(function(){
+       $( "#home" ).addClass('animated bounceOutLeft').one(animateEnd, function(){
+
+       			$( "#cont" ).removeClass("hide").addClass('animated bounceInLeft');	
+       		});
+       });
+    
 
 
+    $("#navhome").click(function(){
+       $( "#home" ).css( "display", "block" );
+    });
+   
+   
+// 
 
 
 
