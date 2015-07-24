63c4a7bb3013d55903c04daf1980d782aed82431
GerardoLSJ <LSJGerardo@gmail.com>
2015-07-23 21:05:10 -0500

    Active Navs

--- main.js
+++ main.js
@@ -1,57 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-/*
-	  $( ".cover" ).on( "mouseover", function() {
-  	$( this ).css( "color", "red" );
-	}); 
+    var animateEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+    $("#navcont").click(function () {
+        $("#home").addClass('animated fadeOut');
+        $("#home").removeClass("animated fadeOut").addClass('hide');
+        $("#cont").removeClass("hide").addClass('animated fadeIn');
+        $("#home").removeClass('animated fadeIn');
+        //active navbar
+        $("#navhome").removeClass("active");
+        $("#navcont").addClass('active');
+    });
 
 
-	$( ".cover" ).on( "mouseleave", function() {
-  	$( this ).css( "color", "green" );
-	}); 
 
-	$( "#feat" ).click(function() {
- 	$( "#home" ).removeClass( "wow fadeInDown" );
-	});
-
-*/ var animateEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-
-	$("#feat").click(function(){
-       $( "#home" ).addClass('animated fadeOut');
-       	$( "#home" ).removeClass("animated fadeOut").addClass('hide');
-       	$( "#cont" ).removeClass("hide").addClass('animated fadeIn');
-       	$( "#home" ).removeClass('animated fadeIn');	
-       });
+    $("#navhome").click(function () {
+        $("#cont").addClass('animated fadeOut');
+        $("#cont").removeClass("animated fadeOut").addClass('hide');
+        $("#home").removeClass("hide").addClass('animated fadeIn');
+        $("#cont").removeClass('animated fadeIn');
+        //active navbar
+        $("#navcont").removeClass("active");
+        $("#navhome").addClass('active');
+    });
     
-
-
-    $("#navhome").click(function(){
-        $( "#cont" ).addClass('animated fadeOut');
-       		$( "#cont" ).removeClass("animated fadeOut").addClass('hide');
-   			$( "#home" ).removeClass("hide").addClass('animated fadeIn');	
-   			$( "#cont" ).removeClass('animated fadeIn');
-       });
-// 
 
 
 
 });
-
-function reflectString(inputString) {
-  
-  var result = '';
-  for (var i = 0; i < inputString.length; i++) {
-    var order = inputString.charCodeAt(i) - 'a'.charCodeAt(0),
-        reflectedCharCode = 'a'.charCodeAt(0) + 25 - order;
-    result += String.fromCharCode(reflectedCharCode);
-  }
-
-  return result;
-}
-
-
-function mixedFractionToImproper(A) {
-
-    var B = [A[1] + A[0] * A[2], A[2]];
-  return B;
-}
