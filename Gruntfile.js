dec96fb91e97c3a9c62a7f27876f09fe28e50773
lsjgerardo <lsjgerardo@gmail.com>
2016-12-04 19:58:59 -0600

    minify more

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,13 @@ module.exports = function (grunt) {
                 mangle: false,
             },
             build: {
-                src: ['js/*.js'],
-                dest: 'build.js'
-            },
+                files: {
+                    'build/customjquery.js': ['js/jquery.min.js', 'js/jquery.slimscroll.min.js', 'js/jquery-ui.min.js', 'js/jquery.fullPage.js'],
+                    'build/build.js': 'js/examples.js',
+                }
 
+
+            }
         },
 
         cssmin: {
@@ -22,7 +25,8 @@ module.exports = function (grunt) {
             },
             target: {
                 files: {
-                    'output.css': ['css/*.css']
+                    'build/output.css': ['css/animate.css','css/bootstrap.min.css','css/examples.css','css/jquery.fullPage.css'],
+                    'build/custom.min.css': ['css/custom.css']
                 }
             }
         }
@@ -32,6 +36,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     // Default task(s).
-    grunt.registerTask('default', ['uglify','cssmin']);
+    grunt.registerTask('default', ['uglify', 'cssmin']);
 
 };
